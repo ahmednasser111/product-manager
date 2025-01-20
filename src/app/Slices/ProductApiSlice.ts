a661bc015e14f7118742e87d00e40d62bef9d7c9
ahmednaser111 <ahmednaser7707@gmail.com>
2025-01-20 14:42:43 +0200

    feat: add pagination support for product API and enhance product queries

--- src/app/Slices/ProductApiSlice.ts
+++ src/app/Slices/ProductApiSlice.ts
@@ -1,20 +1,48 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import axiosInstance from "../../config/axios.config";
+import { Product } from "../../interfaces";
 
 const productApi = axiosInstance.defaults.baseURL + "/api";
 
+interface Response<T> {
+	data: T[];
+	meta: {
+		pagination: {
+			page: number;
+			pageSize: number;
+			pageCount: number;
+			total: number;
+		};
+	};
+}
+
 export const ProductApiSlice = createApi({
 	reducerPath: "product-api",
+	tagTypes: ["Products"],
+	refetchOnReconnect: true,
+	refetchOnFocus: true,
+	refetchOnMountOrArgChange: true,
 	baseQuery: fetchBaseQuery({ baseUrl: productApi }),
 	endpoints: (builder) => ({
-		getProducts: builder.query({
-			query: () => "/products?populate=thumbnail",
+		getProducts: builder.query<Response<Product>, void>({
+			query: () => "/products?populate=thumbnail&populate=categories",
 		}),
-		getProduct: builder.query({
+		getDashboardProductsPaginated: builder.query<
+			Response<Product>,
+			{ page?: number; pageSize?: number }
+		>({
+			query: ({ page = 1, pageSize = 10 }) =>
+				`/products?populate=thumbnail&populate=categories&pagination[page]=${page}&pagination[pageSize]=${pageSize}`,
+		}),
+		getProduct: builder.query<Product, string>({
 			query: (id: string) => `/products/${id}?populate=thumbnail`,
 		}),
 	}),
 });
 
 // Export hooks for the endpoints
-export const { useGetProductsQuery, useGetProductQuery } = ProductApiSlice;
+export const {
+	useGetProductsQuery,
+	useGetProductQuery,
+	useGetDashboardProductsPaginatedQuery,
+} = ProductApiSlice;
